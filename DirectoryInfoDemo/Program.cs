90365eff0e4ff59b9cf406a2876d7fe86bb0375a
umitliguler <info.umitguler@gmail.com>
2018-06-17 11:48:41 +0300

    first

---
+++ DirectoryInfoDemo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DirectoryInfoDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var di = new DirectoryInfo(@"c:\vidobu\");
+
+            Console.WriteLine(di.CreationTime);
+
+            foreach (var dir in di.EnumerateDirectories("*", SearchOption.AllDirectories))
+            {
+                Console.WriteLine(dir.FullName);
+            }
+
+            Console.WriteLine(di.LastAccessTime);
+
+            Console.ReadKey();
+
+        }
+    }
+}
